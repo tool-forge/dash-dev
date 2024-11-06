89fcc2244f6748b3f40adfab3d382821c3a64a53
Ana Berg <ana.berg99@gmail.com>
2024-11-06 20:23:42 -0300

    feat: initial ui

--- src/app/developers/page.tsx
+++ src/app/developers/page.tsx
@@ -1,7 +1,7 @@
 const DevelopersPage = () => {
   return (
-    <main className="w-screen h-[calc(100vh-4rem)] p-12">DevelopersPage'</main>
+    <main className="w-screen h-[calc(100vh-4rem)] p-12">DevelopersPage</main>
   );
 };
 
-export default DevelopersPage';
+export default DevelopersPage;
